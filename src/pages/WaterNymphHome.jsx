b7e439c37de6fc6e61186830c2b6028f39e5306d
Rachel H <rachelhllnd@gmail.com>
2026-01-24 13:56:21 +0000

    File changes

--- src/pages/WaterNymphHome.jsx
+++ src/pages/WaterNymphHome.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { createPageUrl } from '@/utils';
 import { base44 } from '@/api/base44Client';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { motion } from 'framer-motion';
