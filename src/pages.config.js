a3e9cd474ff1778b4bf5209503c5e0290f004af8
Rachel H <rachelhllnd@gmail.com>
2026-01-24 16:04:49 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -17,6 +17,7 @@ import VampireHome from './pages/VampireHome';
 import WaterNymphHome from './pages/WaterNymphHome';
 import WitchHome from './pages/WitchHome';
 import YandereCoupleHome from './pages/YandereCoupleHome';
+import MutantHome from './pages/MutantHome';
 import __Layout from './Layout.jsx';
 
 
@@ -40,6 +41,7 @@ export const PAGES = {
     "WaterNymphHome": WaterNymphHome,
     "WitchHome": WitchHome,
     "YandereCoupleHome": YandereCoupleHome,
+    "MutantHome": MutantHome,
 }
 
 export const pagesConfig = {
